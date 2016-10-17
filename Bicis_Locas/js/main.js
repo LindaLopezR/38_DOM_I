0c2b6e9736d9e03801e12319724cdb9abf7ecd51
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-10-17 13:19:40 +0000

    Arreglo de formulario

--- Bicis_Locas/js/main.js
+++ Bicis_Locas/js/main.js
@@ -1,3 +1,9 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById('name').value;
+	var apellido = document.getElementById('lastname').value;
+	var correo = document.getElementById('input-email').value;
+	var password = document.getElementById('input-password').value;
+	if(nombre === "" || apellido === "" || correo === "" || password === ""){
+		alert('Todos los campos son requeridos');
+	}
 }
--- Traduccion_Formulario/js/main.js
+++ Traduccion_Formulario/js/main.js
@@ -1,26 +1,10 @@
-function cambioIdioma(idioma){
+function cambioIdioma(){
 	var sesion=document.getElementById('form-signin-heading');
 	var remember=document.getElementById('remember');
 	var boton=document.getElementById('boton');
-	if(idioma=='es'){
-		sesion.innerHTML='Por favor inicia sesión';
-		document.getElementById('inputEmail').setAttribute('placeholder', 'Correo Electrónico');
-		document.getElementById('inputPassword').setAttribute('placeholder', 'Contraseña');
-		remember.innerHTML='Recordar datos';
-		boton.innerHTML='Iniciar Sesión';
-	} else if(idioma=='en') {
-		sesion.innerHTML='Please sign in';
-		document.getElementById('inputEmail').setAttribute('placeholder', 'Please enter your email');
-		document.getElementById('inputPassword').setAttribute('placeholder', 'Password');
-		remember.innerHTML='Remember me';
-		boton.innerHTML='Sign in';
-	}
-}
-var botonEs=document.getElementById('btnEs');
-botonEs.onclick=function(){
-	cambioIdioma('es');
-}
-var botonEn=document.getElementById('btnEn');
-botonEn.onclick=function(){
-	cambioIdioma('en');
+	sesion.innerHTML='Por favor inicia sesión';
+	document.getElementById('inputEmail').setAttribute('placeholder', 'Correo Electrónico');
+	document.getElementById('inputPassword').setAttribute('placeholder', 'Contraseña');
+	remember.innerHTML='Recordar datos';
+	boton.innerHTML='Iniciar Sesión';
 }
