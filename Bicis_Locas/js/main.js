a11338e9c5fcd876a79f37b140ca52972a74cf9c
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-10-17 16:52:08 +0000

    Formulario bicicletas

--- Bicis_Locas/js/main.js
+++ Bicis_Locas/js/main.js
@@ -3,7 +3,17 @@ function validateForm(){
 	var apellido = document.getElementById('lastname').value;
 	var correo = document.getElementById('input-email').value;
 	var password = document.getElementById('input-password').value;
+	var indice = document.getElementById("opciones").selectedIndex;
+	expresion = /\w+@+\w+\.+[a-z]/;
 	if(nombre === "" || apellido === "" || correo === "" || password === ""){
 		alert('Todos los campos son requeridos');
+	} else if(!expresion.test(correo)){
+		alert('El correo no es válido');
+	}else if(password.length<6){
+		alert('Contraseña debe tener al menos 6 caracteres');
+	} else if(password==="password" || password==="123456" || password==="098754"){
+		alert('Contraseña no válida');
+	} else if(indice == 0 ) {
+  		alert('Selecciona un tipo de bici');
 	}
 }
