1b6c1cfbb2214d5237d58b97e308deeaced24a94
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-10-18 09:01:53 +0000

    Ajuste de ejercicio

--- Bicis_Locas/js/main.js
+++ Bicis_Locas/js/main.js
@@ -4,12 +4,17 @@ function validateForm(){
 	var correo = document.getElementById('input-email').value;
 	var password = document.getElementById('input-password').value;
 	var indice = document.getElementById("opciones").selectedIndex;
+	letras = /^[A-Z][a-zA-Z]*$/;
 	expresion = /\w+@+\w+\.+[a-z]/;
 	if(nombre === "" || apellido === "" || correo === "" || password === ""){
 		alert('Todos los campos son requeridos');
+	} else if(!letras.test(nombre)){
+		alert('Nombre debe de iniciar con mayúscula');
+	} else if(!letras.test(apellido)){
+		alert('Apellido debe de iniciar con mayúscula');
 	} else if(!expresion.test(correo)){
 		alert('El correo no es válido');
-	}else if(password.length<6){
+	} else if(password.length<6){
 		alert('Contraseña debe tener al menos 6 caracteres');
 	} else if(password==="password" || password==="123456" || password==="098754"){
 		alert('Contraseña no válida');
